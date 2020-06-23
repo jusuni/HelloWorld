a5a747e1d925221c73561e99974cfa1e73f9e3a0
julio <juliosuni@outlook.com>
2020-06-22 21:03:23 -0400

    Added Addnunbers module with a code in it.

---
+++ src/ca/concordia/cce/cejv416/exercises/AddNumbers.java
@@ -0,0 +1,29 @@
+package ca.concordia.cce.cejv416.exercises;
+
+public class AddNumbers {
+
+    // Add two float numbers.
+    public static void main(String args[])
+    {
+        float y = 2F;
+
+
+
+
+        System.out.print(y);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
